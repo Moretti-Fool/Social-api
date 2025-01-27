3b59fd77b5116f0e7cb8d9550e1e0b569e8278d0
Moretti <Moretti-fool>
2025-01-27 22:39:34 +0530

    updated styling and requirements

--- app/routers/user.py
+++ app/routers/user.py
@@ -12,7 +12,8 @@ router = APIRouter(
 
 # USER REGISTRATION
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, 
+                db: Session = Depends(get_db)):
     if user.password != user.confirm_password:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
